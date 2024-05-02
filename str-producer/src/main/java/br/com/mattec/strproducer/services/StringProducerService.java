edef23f2fc501f69d25e9a9e0eceaf62908f2844
jessemusic <jmc_cursos@hotmail.com>
2024-05-02 14:09:18 -0300

    Criando um produtor de mensagens

---
+++ str-producer/src/main/java/br/com/mattec/strproducer/services/StringProducerService.java
@@ -0,0 +1,20 @@
+package br.com.mattec.strproducer.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public  void sendMessage(String message){
+        kafkaTemplate.send("STR-TOPIC", message);
+    }
+
+
+
+}
