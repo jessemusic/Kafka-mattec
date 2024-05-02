3703d9b6d996e41814f2f520b52cb7400194dc59
jessemusic <jmc_cursos@hotmail.com>
2024-05-02 14:28:36 -0300

    Configurando @Log4j2 e partitions e offset das respostas das persistencias das mensagens via post

--- str-producer/src/main/java/br/com/mattec/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/br/com/mattec/strproducer/services/StringProducerService.java
@@ -1,10 +1,13 @@
 package br.com.mattec.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -12,7 +15,17 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public  void sendMessage(String message){
-        kafkaTemplate.send("STR-TOPIC", message);
+        kafkaTemplate.send("STR-TOPIC", message).addCallback(
+                success -> {
+                    if(success != null){
+                        log.info("Send success messages {}",message);
+                        log.info("Partitions {}, Offset {}",success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset());
+
+                    }
+                },
+                error -> log.info("Sent failure messages")
+        );
     }
 
 
